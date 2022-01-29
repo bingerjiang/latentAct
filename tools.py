6c0b300dcc9fac1e77e0a37a1c78ba6324f7a5ac
Binger Jiang <binger.jiang@mail.mcgill.ca>
2022-01-29 11:50:19 +0100

    fix

--- tools.py
+++ tools.py
@@ -121,6 +121,8 @@ def constructPositives (dataset):
 
 #%%
 def constructInputs (prev_sents, curr_sents, next_sents, dataset):
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+
     current_sents_tok = tokenizer(curr_sents, return_tensors='pt', max_length=256, truncation=True, padding='max_length')
     prev_sents_tok = tokenizer(prev_sents, return_tensors='pt', max_length=256, truncation=True, padding='max_length')
     next_sents_tok = tokenizer(next_sents, return_tensors='pt', max_length=256, truncation=True, padding='max_length')
