0eac01f8f3bab7a9a0b2e6469518d76fad2c7d93
bingerjiang <binger.jiang@mail.mcgill.ca>
2022-02-21 14:27:24 +0100

    added --pre_z_tanh

--- models.py
+++ models.py
@@ -189,12 +189,100 @@ class BertForForwardBackward_binary_flex(BertPreTrainedModel):
         self.z_forward = nn.Linear(config.hidden_size, config.FB_function_size)
         self.z_backward = nn.Linear(config.hidden_size, config.FB_function_size)
         self.cls = nn.Linear(config.FB_function_size*2, 2)
-
+        self.activation = nn.Tanh()
+        self.pre_z_tanh = config.pre_z_tanh
         # Initialize weights and apply final processing
         #self.post_init()
-
+        
     #@add_start_docstrings_to_model_forward(BERT_INPUTS_DOCSTRING.format("batch_size, sequence_length"))
     #@replace_return_docstrings(output_type=NextSentencePredictorOutput, config_class=_CONFIG_FOR_DOC)
+    def forward(
+        self,
+        
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+        **kwargs,
+    ):
+        
+
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        prev_sents_ids,curr_sents_ids, next_sents_ids = input_ids[0], input_ids[1], input_ids[2]
+        prev_attention_mask, curr_attention_mask, next_attention_mask = attention_mask[0], attention_mask[1],attention_mask[2]
+        prev_type_ids, curr_type_ids, next_type_ids = token_type_ids[0], token_type_ids[1], token_type_ids[2]
+
+        # TODO: check input argument
+        prev_out = self.bert(prev_sents_ids,
+                                          attention_mask = prev_attention_mask,
+                                          token_type_ids = prev_type_ids)
+        next_out = self.bert(next_sents_ids,
+                                          attention_mask = next_attention_mask,
+                                          token_type_ids = next_type_ids)
+        curr_out = self.bert(curr_sents_ids,
+                                          attention_mask = curr_attention_mask,
+                                          token_type_ids = curr_type_ids)
+        
+        prev_last_hid, prev_pooler = prev_out['last_hidden_state'],prev_out['pooler_output']
+        next_last_hid, next_pooler = next_out['last_hidden_state'], next_out['pooler_output']
+        curr_last_hid, curr_pooler = curr_out['last_hidden_state'], curr_out['pooler_output']
+        
+        if self.pre_z_tanh:
+            prev_pooler = self.activation(prev_pooler)
+            curr_pooler = self.activation(curr_pooler)
+            next_pooler = self.activation(next_pooler)
+        
+        ## get forward function and backward function
+        prev_forward =self.z_forward(prev_pooler)
+        curr_backward = self.z_backward(curr_pooler)
+        curr_forward = self.z_forward(curr_pooler)
+        next_backward = self.z_backward(next_pooler)
+        
+        forward_pooled_outs = torch.cat((prev_forward, curr_forward), 0)
+        backward_pooled_outs = torch.cat((curr_backward, next_backward), 0)
+        pooled_outs = torch.cat((forward_pooled_outs, backward_pooled_outs),1)
+        
+        labels = labels.repeat(2,1)
+
+        seq_relationship_scores = self.cls(pooled_outs)
+        #pdb.set_trace()
+        forward_backward_loss = None
+        if labels is not None:
+            loss_fct = CrossEntropyLoss()
+            forward_backward_loss = loss_fct(seq_relationship_scores.view(-1, 2), labels.view(-1))
+        #pdb.set_trace()
+        return NextSentencePredictorOutput(
+            loss=forward_backward_loss,
+            logits=seq_relationship_scores,
+            hidden_states=[prev_forward, curr_forward, curr_backward, next_backward],
+            #attentions=prev_outs.attentions,
+        )
+    
+class BertForForwardBackward_binary_flex_tanh(BertPreTrainedModel):
+    
+    '''
+    Same with bertforforwardbackward_binary_flex,
+    added tanh before feeding to z_function
+    '''
+    def __init__(self, config):
+        super().__init__(config)
+
+        # self.bert = BertModel(config)
+        
+        #config = BertConfig.from_pretrained('bert-base-uncased')    
+        self.bert = AutoModel.from_config(config)
+        self.z_forward = nn.Linear(config.hidden_size, config.FB_function_size)
+        self.z_backward = nn.Linear(config.hidden_size, config.FB_function_size)
+        self.cls = nn.Linear(config.FB_function_size*2, 2)
+        self.activation = nn.Tanh()
+
     def forward(
         self,
         
@@ -234,10 +322,10 @@ class BertForForwardBackward_binary_flex(BertPreTrainedModel):
         curr_last_hid, curr_pooler = curr_out['last_hidden_state'], curr_out['pooler_output']
         
         ## get forward function and backward function
-        prev_forward =self.z_forward(prev_pooler)
-        curr_backward = self.z_backward(curr_pooler)
-        curr_forward = self.z_forward(curr_pooler)
-        next_backward = self.z_backward(next_pooler)
+        prev_forward =self.z_forward(self.activation(prev_pooler))
+        curr_backward = self.z_backward(self.activation(curr_pooler))
+        curr_forward = self.z_forward(self.activation(curr_pooler))
+        next_backward = self.z_backward(self.activation(next_pooler))
         
         forward_pooled_outs = torch.cat((prev_forward, curr_forward), 0)
         backward_pooled_outs = torch.cat((curr_backward, next_backward), 0)
@@ -258,7 +346,6 @@ class BertForForwardBackward_binary_flex(BertPreTrainedModel):
             hidden_states=[prev_forward, curr_forward, curr_backward, next_backward],
             #attentions=prev_outs.attentions,
         )
-    
         
 class BertForForwardBackwardPrediction_cos(BertPreTrainedModel):
     def __init__(self, config):
@@ -366,9 +453,10 @@ class BertForForwardBackward_cos_flex(BertPreTrainedModel):
         self.z_forward = nn.Linear(config.hidden_size, config.FB_function_size)
         self.z_backward = nn.Linear(config.hidden_size, config.FB_function_size)
         #self.cls = nn.Linear(config.hidden_size*2, 2)
-
+        self.pre_z_tanh = config.pre_z_tanh
         # Initialize weights and apply final processing
         #self.post_init()
+        self.activation = nn.Tanh()
 
     #@add_start_docstrings_to_model_forward(BERT_INPUTS_DOCSTRING.format("batch_size, sequence_length"))
     #@replace_return_docstrings(output_type=NextSentencePredictorOutput, config_class=_CONFIG_FOR_DOC)
@@ -408,6 +496,11 @@ class BertForForwardBackward_cos_flex(BertPreTrainedModel):
         prev_last_hid, prev_pooler = prev_out['last_hidden_state'],prev_out['pooler_output']
         next_last_hid, next_pooler = next_out['last_hidden_state'], next_out['pooler_output']
         curr_last_hid, curr_pooler = curr_out['last_hidden_state'], curr_out['pooler_output']
+        
+        if self.pre_z_tanh:
+            prev_pooler = self.activation(prev_pooler)
+            curr_pooler = self.activation(curr_pooler)
+            next_pooler = self.activation(next_pooler)
         
         ## get forward function and backward function
         prev_forward =self.z_forward(prev_pooler)
@@ -559,7 +652,8 @@ class BertForForwardBackward_cos_tlayer_flex(BertPreTrainedModel):
         self.z_backward = nn.Linear(config.hidden_size, config.FB_function_size)
         #self.cls = nn.Linear(config.hidden_size*2, 2)
         self.inter_rep = nn.TransformerEncoderLayer(d_model=config.FB_function_size, nhead=4)
-
+        self.pre_z_tanh = config.pre_z_tanh
+        self.activation = nn.Tanh()
         # Initialize weights and apply final processing
         #self.post_init()
 
@@ -602,6 +696,10 @@ class BertForForwardBackward_cos_tlayer_flex(BertPreTrainedModel):
         next_last_hid, next_pooler = next_out['last_hidden_state'], next_out['pooler_output']
         curr_last_hid, curr_pooler = curr_out['last_hidden_state'], curr_out['pooler_output']
         
+        if self.pre_z_tanh:
+            prev_pooler = self.activation(prev_pooler)
+            curr_pooler = self.activation(curr_pooler)
+            next_pooler = self.activation(next_pooler)
         ## get forward function and backward function
         prev_forward =self.z_forward(prev_pooler)
         curr_backward = self.z_backward(curr_pooler)
