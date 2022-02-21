6688f0dd68eb120d693888f4f5fc04f8f9590ae1
bingerjiang <binger.jiang@mail.mcgill.ca>
2022-02-21 14:28:46 +0100

    update

--- run_train_flex.sh
+++ run_train_flex.sh
@@ -32,14 +32,92 @@
 #                       --flex\
 #                       --FB_function_size 60\
 
-export CUDA_VISIBLE_DEVICES=5
+# 0213 train 2 binary 2 costlayer
+# export CUDA_VISIBLE_DEVICES=3
+# python train_with_args.py --k_neg_pos 10 \
+#                       --n_epochs 100\
+#                       --eval_batch_size 4\
+#                       --train_batch_size 1\
+#                       --model_type 'binary'\
+#                       --lr 1e-5\
+#                       --save_model_dir '../binary_state_dicts/'\
+#                       --flex\
+#                       --FB_function_size 4\
+
+# export CUDA_VISIBLE_DEVICES=4
+# python train_with_args.py --k_neg_pos 10 \
+#                       --n_epochs 100\
+#                       --eval_batch_size 4\
+#                       --train_batch_size 1\
+#                       --model_type 'binary'\
+#                       --lr 5e-5\
+#                       --save_model_dir '../binary_state_dicts/'\
+#                       --flex\
+#                       --FB_function_size 4\
+# export CUDA_VISIBLE_DEVICES=5
+# python train_with_args.py --k_neg_pos 10 \
+#                       --n_epochs 100\
+#                       --eval_batch_size 4\
+#                       --train_batch_size 1\
+#                       --model_type 'cos_tlayer'\
+#                       --lr 1e-5\
+#                       --save_model_dir '../costlayer_state_dicts/'\
+#                       --flex\
+#                       --FB_function_size 4\           
+# export CUDA_VISIBLE_DEVICES=7
+# python train_with_args.py --k_neg_pos 10 \
+#                       --n_epochs 100\
+#                       --eval_batch_size 4\
+#                       --train_batch_size 1\
+#                       --model_type 'cos_tlayer'\
+#                       --lr 5e-5\
+#                       --save_model_dir '../costlayer_state_dicts/'\
+#                       --flex\
+#                       --FB_function_size 4\
+# export CUDA_VISIBLE_DEVICES=5
+# python train_with_args.py --k_neg_pos 10 \
+#                       --n_epochs 30\
+#                       --eval_batch_size 4\
+#                       --train_batch_size 1\
+#                       --model_type 'binary'\
+#                       --lr 1e-5\
+#                       --save_model_dir '../binary_state_dicts/'\
+#                       --flex\
+#                       --FB_function_size 32\
+#                       --pre_z_tanh
+
+# export CUDA_VISIBLE_DEVICES=7
+# python train_with_args.py --k_neg_pos 10 \
+#                       --n_epochs 30\
+#                       --eval_batch_size 4\
+#                       --train_batch_size 1\
+#                       --model_type 'binary'\
+#                       --lr 5e-5\
+#                       --save_model_dir '../binary_state_dicts/'\
+#                       --flex\
+#                       --FB_function_size 32\        
+#                       --pre_z_tanh 
+
+# export CUDA_VISIBLE_DEVICES=3
+# python train_with_args.py --k_neg_pos 10 \
+#                       --n_epochs 30\
+#                       --eval_batch_size 4\
+#                       --train_batch_size 1\
+#                       --model_type 'cos_tlayer'\
+#                       --lr 1e-5\
+#                       --save_model_dir '../costlayer_state_dicts/'\
+#                       --flex \
+#                       --FB_function_size 32 \
+#                       --pre_z_tanh 
+
+export CUDA_VISIBLE_DEVICES=4
 python train_with_args.py --k_neg_pos 10 \
-                      --n_epochs 1\
+                      --n_epochs 30\
                       --eval_batch_size 4\
                       --train_batch_size 1\
-                      --model_type 'cos'\
-                      --lr 1e-5\
-                      --save_model_dir '../cos_state_dicts/'\
+                      --model_type 'cos_tlayer'\
+                      --lr 5e-5\
+                      --save_model_dir '../costlayer_state_dicts/'\
                       --flex\
-                      --FB_function_size 20\
-                      --trial
+                      --FB_function_size 32\        
+                      --pre_z_tanh 
